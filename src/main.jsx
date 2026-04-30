165a7c648c5a40ff94dda0447ca36e30f901f288
elnobrianardi <elno.brianardi@gmail.com>
2026-04-30 11:02:29 +0700

    feat: integrate Lenis for smooth scrolling and add related styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ThemeProvider } from '@material-tailwind/react'
+import {ReactLenis} from 'lenis/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider>
-    <App />
-    </ThemeProvider>
+    <ReactLenis root>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </ReactLenis>
   </StrictMode>,
 )
